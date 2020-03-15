b4f0ff725ed859aaa26a511b173b1c82aae5fe2f
nikolaos_sergevidis <nikolaos.sergevidis@gmail.com>
2020-03-15 23:26:21 +0200

    Added my first model

--- portofolio/models.py
+++ portofolio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    """docstring for project."""
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image=models.ImageField(upload_to='portofolio/images')
+    url = models.URLField(blank=True)
+    
+
+    def __init__(self, arg):
+        super(project, self).__init__()
+        self.arg = arg
