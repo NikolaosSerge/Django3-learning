07ecbc85fb88c49751a84f8be92536bc1c658636
nikolaos_sergevidis <nikolaos.sergevidis@gmail.com>
2020-03-17 01:40:25 +0200

    Left at video 47 of the tutorial, I have to look into the issue of generating an link for my every blogpost!

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
-# Create your models here.
+# Dont forget to make migrations and to migrate!!!
+class Blog(models.Model):
+    """docstring for Blogs."""
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    description = models.TextField()
+    URL = models.URLField(blank=True)
+    def __str__(self):
+        return self.title
--- portofolio/views.py
+++ portofolio/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Project
+
 
 def HomePage(request):
-    return render(request,"portofolio/index.html")
+    projects = Project.objects.all()
+    return render(request,"portofolio/index.html",{'projects': projects})
