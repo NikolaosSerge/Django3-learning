db09f07182f65499825bf815b213088dfd82dc52
nikolaos_sergevidis <nikolaos.sergevidis@gmail.com>
2020-03-16 02:11:41 +0200

    Before sleep

--- personal_portofolio/urls.py
+++ personal_portofolio/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from portofolio import views
+
 urlpatterns = [
+    path('', views.HomePage, name = "home"),
     path('admin/', admin.site.urls),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
