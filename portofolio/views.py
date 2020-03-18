be6fa92d93893418452aa79d545c55142763c197
nikolaos_sergevidis <nikolaos.sergevidis@gmail.com>
2020-03-19 01:58:30 +0200

    Day 3 styling my portofolio!

--- portofolio/views.py
+++ portofolio/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Project
 
 
+
 def HomePage(request):
     projects = Project.objects.all()
     return render(request,"portofolio/index.html",{'projects': projects})
